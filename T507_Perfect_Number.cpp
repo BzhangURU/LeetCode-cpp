8f8e6d1fef1c40512c16ca14bcf8d6d26830dbb0
Barton <bodong.zhang@yahoo.com>
2017-04-23 15:35:36 -0600

    Update T507_Perfect_Number.cpp

--- T507_Perfect_Number.cpp
+++ T507_Perfect_Number.cpp
@@ -6,3 +6,25 @@ Input: 28
 Output: True
 Explanation: 28 = 1 + 2 + 4 + 7 + 14
 Note: The input number n will not exceed 100,000,000. (1e8)*/
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num<=0)
+    		return false;
+    	if(num==1)
+    		return false;
+    	int sum=1;
+    
+    	int squareRoot=(int)sqrt((double)num);
+    	int divisor2;
+    	for(int i=2;i<=squareRoot;i++){
+    		if(num%i==0){
+    			divisor2=num/i;
+    			sum+=i;
+    			if(divisor2!=i)
+    				sum+=divisor2;
+    		}
+    	}
+    	return sum==num;
+    }
+};
