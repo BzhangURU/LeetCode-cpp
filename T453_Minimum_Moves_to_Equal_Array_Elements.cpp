3f1a5672f85a036ad2185e5bc8f00d5d8d4faf85
Barton <bodong.zhang@yahoo.com>
2017-04-25 00:05:38 -0600

    Update T453_Minimum_Moves_to_Equal_Array_Elements.cpp

--- T453_Minimum_Moves_to_Equal_Array_Elements.cpp
+++ T453_Minimum_Moves_to_Equal_Array_Elements.cpp
@@ -13,3 +13,17 @@ Explanation:
 Only three moves are needed (remember each move increments two elements):
 
 [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]*/
+int minMoves(vector<int>& nums) {
+	if(nums.size()<=1)
+		return 0;
+	long long num0=nums[0];
+	long long min=INT_MAX;
+	long long sum=0;
+	for(int i=1;i<nums.size();i++){
+		sum+=(long long)nums[i]-num0;
+		if(min>nums[i])
+			min=nums[i];
+	}
+	sum+=(num0-min)*nums.size();
+	return sum;
+}
